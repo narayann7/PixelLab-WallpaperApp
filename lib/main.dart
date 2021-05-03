0adae12986109175824d01721371a356386e3091
narayann7 <laxminarayanreddy432@gmail.com>
2021-05-02 20:00:04 -0400

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,21 +14,10 @@ class Wallx extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       theme: ThemeData(
-        // Define the default brightness and colors.
         brightness: Brightness.dark,
         primaryColor: b1,
         accentColor: g4,
-
-        // Define the default font family.
         fontFamily: 'Cabin',
-
-        // Define the default TextTheme. Use this to specify the default
-        // text styling for headlines, titles, bodies of text, and more.
-        // textTheme: TextTheme(
-        //   headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-        //   headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-        //   bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-        // ),
       ),
       debugShowCheckedModeBanner: false,
       home: Home(),
